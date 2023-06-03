78ee8e74e1beeb36746f72ffb4651b7961150429
JohnA98 <111317228+JohnA98@users.noreply.github.com>
2023-06-02 23:38:04 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Work-Day-Scheduler
-Calendar app that users can save events in.
+
+## Description
+
+This website is a work scheduler. If the user wants to input events that they want to do during work hours, they would type in whatever event they are planning or have already done in the correct time slot, then hit the save button. My motivation for this project comes from the fact that sometimes I can get off track, and not complete the work that I have to do in the correct time frame, so this application could really help someone like myself. I built this project so others like myself can focus and plan out their time better. This was the first project that utilized third-party APIs as well as the library Dayjs. The events are kept even when the user leaves the page and comes back, so that if they plan out their work day and forget about it, they can still come back to it if need be.
+
+## Installation
+
+N/A
+
+## Usage
+
+Landingpic
+
+To use this website, simply fill in information on what events you would like to do throughout your work day. They can consist of meetings, actual work, and even lunch breaks.
+Filling-in-work
+
+From there, hit the save button to the right and the information is saved to the local storage, so if you ever leave the page, you can still have your events available.
+
+## Credits
+
+https://www.w3schools.com/jquery/jquery_get_started.asp
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+
--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Selecting all the elements.
+var timeNowEl = document.querySelector('#currentDay');
+var timeBlockEl = document.querySelector('.time-block');
+var timeContainer = document.querySelector("#time-container");
+var saveBtnEl = document.querySelector("button");
+var textareaEl = document.querySelectorAll("textarea");
+
+// Shows day, month, year
+var timeNow = setInterval(function() {
+  var dayMonthYear = dayjs().format('dddd, MMMM D, YYYY');
+  timeNowEl.textContent = dayMonthYear;
+}, 1000);
+
+// Shows boxes separated by 1 hour
+var timeHour = dayjs().hour();
+// var i is the number hour in the day, j is the number slot in the calendar. 
+// This goes through to make whichever slot the correct color if it is equal, ahead, or behind in the day.
+for (var i = 9, j = 0; i < 18; i++, j++) {
+  if (timeHour === i) {
+    timeBlockEl[i].children[1].setAttribute("class", "description present");        
+  }
+  else if (timeHour < i) {
+    timeBlockEl[i].children[1].setAttribute("class", "description future");        
+  }
+  else {
+    timeBlockEl[i].children[1].setAttribute("class", "description past");        
+  }
+}
+
+//click event when the user goes to enter an event.
+timeContainer.addEventListener("click", (event) => {
+  target = event.target;
+
+ 
+  if (target == target.parentElement.children[2]) {
+    console.log(target.parentElement.children[1].value);
+    localStorage.setItem(
+      target.parentElement.children[0].textContent,
+      target.parentElement.children[1].value
+    );
+
+    // if user clicks the save button icon, text and time is saved in localstorage
+  } else if (target.tagName == "I") {
+    console.log(target.parentElement.parentElement.children[1].value);
+    localStorage.setItem(
+      target.parentElement.parentElement.children[0].textContent,
+      target.parentElement.parentElement.children[1].value
+    );
+  } else return;
 });
+
+// Saves the values in the slots to the local storage
+textareaEl[0].textContent = localStorage.getItem("9AM");
+textareaEl[1].textContent = localStorage.getItem("10AM");
+textareaEl[2].textContent = localStorage.getItem("11AM");
+textareaEl[3].textContent = localStorage.getItem("12PM");
+textareaEl[4].textContent = localStorage.getItem("1PM");
+textareaEl[5].textContent = localStorage.getItem("2PM");
+textareaEl[6].textContent = localStorage.getItem("3PM");
+textareaEl[7].textContent = localStorage.getItem("4PM");
+textareaEl[8].textContent = localStorage.getItem("5PM");
